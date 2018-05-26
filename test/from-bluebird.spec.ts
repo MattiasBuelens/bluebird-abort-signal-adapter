2d53f7ce105f582264ca377dcf8b55c9575fdb44
Mattias Buelens <mattias@buelens.com>
2018-05-26 22:13:33 +0200

    More tests

--- test/from-bluebird.spec.ts
+++ test/from-bluebird.spec.ts
@@ -9,7 +9,26 @@ describe('fromBluebird', () => {
         const {promise, controller} = fromBluebird(input);
 
         expect(controller.signal.aborted).toBe(false);
-        const state = await inspectPromise(promise);
-        expect(state).toBe(PromiseState.PENDING);
-    })
+        expect(await inspectPromise(promise)).toBe(PromiseState.PENDING);
+    });
+
+    it('returns a fulfilled promise and non-aborted controller when given a fulfilled promise', async () => {
+        const value = 'yay';
+        const input = Bluebird.resolve(value);
+
+        const {promise, controller} = fromBluebird(input);
+
+        expect(controller.signal.aborted).toBe(false);
+        await expect(promise).resolves.toBe(value);
+    });
+
+    it('returns a rejected promise and non-aborted controller when given a rejected promise', async () => {
+        const reason = 'boo';
+        const input = Bluebird.reject(reason);
+
+        const {promise, controller} = fromBluebird(input);
+
+        expect(controller.signal.aborted).toBe(false);
+        await expect(promise).rejects.toBe(reason);
+    });
 });
