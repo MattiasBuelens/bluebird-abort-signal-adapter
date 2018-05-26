7c4406d3e609064f824aa83e55fc0dcf0c7f8057
Mattias Buelens <mattias@buelens.com>
2018-05-26 22:02:01 +0200

    Assert promise state

--- test/from-bluebird.spec.ts
+++ test/from-bluebird.spec.ts
@@ -1,13 +1,15 @@
 import Bluebird from 'bluebird';
 import {fromBluebird} from '../src';
-import {noop} from './test-utils';
+import {inspectPromise, noop, PromiseState} from './test-utils';
 
 describe('fromBluebird', () => {
-    it('returns a pending promise and non-aborted signal when given a pending promise', () => {
+    it('returns a pending promise and non-aborted signal when given a pending promise', async () => {
         const input = new Bluebird(noop);
 
         const {promise, controller} = fromBluebird(input);
 
         expect(controller.signal.aborted).toBe(false);
+        const state = await inspectPromise(promise);
+        expect(state).toBe(PromiseState.PENDING);
     })
 });
--- test/test-utils.ts
+++ test/test-utils.ts
@@ -1,3 +1,23 @@
 export function noop() {
     return;
 }
+
+export function delay<T>(value: T, timeout: number): Promise<T> {
+    return new Promise<T>(resolve => setTimeout(() => resolve(value), timeout));
+}
+
+export enum PromiseState {
+    PENDING = 'pending',
+    FULFILLED = 'fulfilled',
+    REJECTED = 'rejected'
+}
+
+export function inspectPromise(promise: PromiseLike<any>): Promise<PromiseState> {
+    const sentinel = {};
+    // Assume that the promise won't settle before the next turn of the event loop
+    return Promise.race([promise, delay(sentinel, 0)])
+        .then(
+            value => (value === sentinel) ? PromiseState.PENDING : PromiseState.FULFILLED,
+            () => PromiseState.REJECTED
+        );
+}
